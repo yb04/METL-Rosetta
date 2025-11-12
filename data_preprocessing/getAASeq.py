3718c2b15852c1fdd1cc7f2fd1ec36961fb768ef
JohannesWolz2002 <dadowolz@gmail.com>
2025-11-12 18:05:01 +0100

    Add files via upload

--- data_preprocessing/getAASeq.py
+++ data_preprocessing/getAASeq.py
@@ -1,69 +1,51 @@
 import requests
+import pandas as pd
 
-pdb_id = "1h2e"
+pdb_id = "1A3A"
 
-url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
-filePDB = requests.get(url).text.splitlines()
-seq = []
-for line in filePDB:
-    if line.startswith("SEQRES"):
-        if str(line[11])== "A":
-            seq.append(str(line[19:].strip()))
+def get_AA(pdbId):
+    url = f"https://files.rcsb.org/download/{pdbId}.pdb"
+    filePDB = requests.get(url).text.splitlines()
+    seq = []
+    for line in filePDB:
+        if line.startswith("SEQRES"):
+            if str(line[11]) == "A":
+                seq.append(str(line[19:].strip()))
+    result = convert_to_single(seq)
+    return result
 
 def convert_to_single(seqeunce):
-    result = []
+    bitch = []
     for entry in seqeunce:
         parts = entry.split(" ")
         for part in parts:
-            result.append(three_to_one_amino_acid_code(part))
-    string = "".join(result)
+            bitch.append(three_to_one_amino_acid_code(part))
+    string = "".join(bitch)
     return string
 
 def three_to_one_amino_acid_code(three_letter_code):
     mapping = {
-        'ALA': 'A',  # Alanin
-        'ARG': 'R',  # Arginin
-        'ASN': 'N',  # Asparagin
-        'ASP': 'D',  # Asparaginsäure
-        'CYS': 'C',  # Cystein
-        'GLU': 'E',  # Glutaminsäure
-        'GLN': 'Q',  # Glutamin
-        'GLY': 'G',  # Glycin
-        'HIS': 'H',  # Histidin
-        'ILE': 'I',  # Isoleucin
-        'LEU': 'L',  # Leucin
-        'LYS': 'K',  # Lysin
-        'MET': 'M',  # Methionin
-        'PHE': 'F',  # Phenylalanin
-        'PRO': 'P',  # Prolin
-        'SER': 'S',  # Serin
-        'THR': 'T',  # Threonin
-        'TRP': 'W',  # Tryptophan
-        'TYR': 'Y',  # Tyrosin
-        'VAL': 'V'  # Valin
+        'ALA': 'A',
+        'ARG': 'R',
+        'ASN': 'N',
+        'ASP': 'D',
+        'CYS': 'C',
+        'GLU': 'E',
+        'GLN': 'Q',
+        'GLY': 'G',
+        'HIS': 'H',
+        'ILE': 'I',
+        'LEU': 'L',
+        'LYS': 'K',
+        'MET': 'M',
+        'PHE': 'F',
+        'PRO': 'P',
+        'SER': 'S',
+        'THR': 'T',
+        'TRP': 'W',
+        'TYR': 'Y',
+        'VAL': 'V'
     }
     upper_code = three_letter_code.upper()
     one_letter_code = mapping.get(upper_code, '?')
     return one_letter_code
-
-result = convert_to_single(seq)
-print(result)
-
-def apply_mutations(seq, muts):
-    # seq: str mit 1-basiertem Index (biologische Konvention)
-    # muts: z.B. "A123V" oder Liste ["K31G","I102W"]
-    if isinstance(muts, str):
-        muts = [muts]
-    seq_list = list(seq)
-    for m in muts:
-        ref = m[0]
-        pos = int(m[1:-1])
-        alt = m[-1]
-        if seq_list[pos-1] != ref:
-            raise ValueError(f"Ref-Mismatch an {pos}: erwartet {ref}, gefunden {seq_list[pos-1]}")
-        seq_list[pos-1] = alt
-    return "".join(seq_list)
-
-
-mut = apply_mutations(result, ["I147H","E184Y","D197M"])
-print(mut)
